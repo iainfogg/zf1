c9960fe4f3820d04042ad3337e1d166b83365da1
iainfogg <iain@feedthemouse.com>
2018-03-07 16:51:04 +0000

    Update Backend, add missing {

--- library/Zend/Cache/Backend.php
+++ library/Zend/Cache/Backend.php
@@ -76,7 +76,7 @@ class Zend_Cache_Backend
     public function setDirectives($directives)
     {
         if (!is_array($directives)) Zend_Cache::throwException('Directives parameter must be an array');
-        foreach ($directives as $name => $value)
+        foreach ($directives as $name => $value) {
             if (!is_string($name)) {
                 Zend_Cache::throwException("Incorrect option name : $name");
             }
